4e9357004676cd1dae98ffda63e96568e8d99a7e
heziming_o <heziming_o@163.com>
2026-08-06 20:27:02 +0800

    fix：测试打包

--- src/apis/httpInstance.ts
+++ src/apis/httpInstance.ts
@@ -7,7 +7,7 @@ const httpInstance = axios.create({
     timeout: 60000, // 默认超时时间
 });
 
-export const sbVersion = '26.07.30';
+export const sbVersion = '26.07.31';
 
 /**
  * 后端使用siteToken来统计UV PV IP日均 
