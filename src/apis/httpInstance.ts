8f2f043a11b7bd4a3c09724efb5a6ce7073d9d47
heziming_o <heziming_o@163.com>
2026-08-12 02:47:41 +0000

    fix：修改样式遮挡按钮

--- src/apis/httpInstance.ts
+++ src/apis/httpInstance.ts
@@ -7,7 +7,7 @@ const httpInstance = axios.create({
     timeout: 60000, // 默认超时时间
 });
 
-export const sbVersion = '26.08.11';
+export const sbVersion = '26.08.12';
 
 /**
  * 后端使用siteToken来统计UV PV IP日均 
