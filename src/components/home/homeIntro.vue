2f1af1a05317cc8e1e8c210a8631f96a38c3cf25
Miyang0164 <127235514+Miyang0164@users.noreply.github.com>
2026-08-17 19:36:05 +0800

    fix: gate install page and route home download link

--- src/components/home/homeIntro.vue
+++ src/components/home/homeIntro.vue
@@ -11,7 +11,7 @@
             <b>
                 <RouterLink to="/Tampermonkey">油猴插件</RouterLink>
                 (已支持斗鱼新UI)
-                <a href="https://dgq63136.cn/dgq63136.user.js" target="_blank">点击下载</a>
+                <RouterLink to="/Tampermonkey">点击下载</RouterLink>
                 可在斗鱼直播间一键发送烂梗
             </b>
         </p>
